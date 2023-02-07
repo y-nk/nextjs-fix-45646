cd85110a47d5e957b3832147017f3b4f891e4835
Julien Barbay <julien.barbay@gmail.com>
2023-02-07 12:01:33 +0700

    final intention example

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,34 @@
+import { dirname, join } from "path";
+import glob from "fast-glob";
+
 import Link from "next/link";
 
-export default function Home() {
+const __dirname = dirname(new URL(import.meta.url).pathname)
+
+async function getPosts() {
+  const posts = await glob(join(__dirname, '**/page*'))
+
+  const slugs = posts
+    .filter(absolutePath => process.env.NODE_ENV !== 'production' || !absolutePath.includes('(drafts)'))
+    .map(absolutePath => absolutePath.replace(`${__dirname}/`, '')) // root path
+    .map(relativePath => relativePath.replace(/page.*$/, '')) // page file
+    .map(slug => slug.replace(/\(.*\)\//, '')) // route group
+    .filter(Boolean)
+
+  return slugs
+}
+
+
+export default async function Home() {
+  const posts = await getPosts()
+  console.log({ posts })
+
   return (
     <main>
       <ul>
-        <li><Link href="/article-1">published</Link></li>
-        <li><Link href="/article-2">draft</Link></li>
+        {posts.map(slug => (
+          <li key={slug}><Link href={slug}>{slug}</Link></li>
+        ))}
       </ul>
     </main>
   )
